0bc65f17df1999131ba4dad20ca89990ea4f4e70
nrcool <nomanalam7214@gmail.com>
2022-07-21 10:09:17 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -9,6 +9,23 @@ const Alert = () => {
   };
   //your component here
 
+  return (
+
+    <>
+    {colorClasses.color==="red"?  <div class="alert alert-danger" role="alert">
+        OMG! Something really bad has happended!
+    </div>:  <div class="alert alert-warning" role="alert">
+      Well, it is not that bad after all!
+    </div> }
+    </>
+
+   /*  <div className={ `alert alert-${colorClasses.color==="red"?"danger":"warning"}` }  role="alert">
+    {colorClasses.color==="red"? colorClasses.text4Red: colorClasses.text4Orange}
+
+      </div> */
+  )
+
+
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
